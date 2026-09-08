27286d857cbcc302bb5505ff594fbcff5d804c17
kiziunderscore <ikizyon@gmail.com>
2026-09-08 02:18:55 -0400

    Component struct fixes and documentation

--- app/src/main/cpp/caver/Component.h
+++ app/src/main/cpp/caver/Component.h
@@ -4,19 +4,24 @@
 #include "stdstring.h"
 #include "SceneObject.h"
 
+/*
+ * Components are present within SceneObjects and
+ * usually define the behaviour and utilities of an object.
+ *
+ * They can be accessed via
+ * `SceneObject_ComponentWithInterface` using an interface handle,
+ * `component_fetch` using the component's name like `ModelComponent`.
+ */
 typedef struct Component {
-	void *vtable;
-	char _pad0[archSplit(0x04, 0x08)];
-	void *vtable2;
-	void *dat;
-	int flags;
-	int flags2;
-	SceneObject *object;
-	char _pad1[archSplit(0x04, 0x08)];
-	String label; // needs further testing...
-	void *outletBindingsRoot;
-	char _pad2[archSplit(0x04, 0x08)];
-	long outletBindingsCount;
+	void *vtable; // 0, 0
+	int unknown0; // 4, 8
+	char _pad0[archSplit(0x0, 0x4)]; // 8, 12
+	void *vtable2; // 8, 16
+	void *dat; // 12, 24
+	int flags; // 16, 32
+	int flags1; // 20, 36
+	SceneObject *object; // 24 (0x18), 40 (0x28)
+	String label; // 28 (0x1c), 48 (0x30)
 } Component; // sizeof should be 0x28, 0x48
 // this = operator_new(0x28); Component::Component(this);
 // this_00 = operator_new(0x48); Component::Component(this_00);
