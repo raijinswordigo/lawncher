b70379c1eeda7fc75a7a9a4819c91c4c4d048b9f
kiziunderscore <ikizyon@gmail.com>
2026-09-08 02:18:25 -0400

    Fix the type to Component

--- app/src/main/cpp/hooks/Component.c
+++ app/src/main/cpp/hooks/Component.c
@@ -14,7 +14,7 @@ Component* component_fetch(SceneObject *obj, const char *component_name) {
 	snprintf(interface_id, sizeof(interface_id), "_ZN5Caver%d%s9InterfaceEv", len, component_name);
 	void *interface = swordigo_dlsym(interface_id);
 	if (!interface) return NULL;
-	void *comp = SceneObject_ComponentWithInterface(obj, interface);
+	Component *comp = SceneObject_ComponentWithInterface(obj, interface);
 	if (!comp) return NULL;
 	LOGD("Fetched a component: %s", component_name);
 
