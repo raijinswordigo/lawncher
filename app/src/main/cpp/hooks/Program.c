d376bb5188121a3a170ac5ad5e94b3a0e0fe9a0e
kiziunderscore <ikizyon@gmail.com>
2026-09-10 04:25:22 -0400

    Symbol fixes

--- app/src/main/cpp/hooks/Program.c
+++ app/src/main/cpp/hooks/Program.c
@@ -1,19 +1,21 @@
 #include "Program.h"
+#include "log.h"
+#define LOG_TAG "CaverProgram"
 
 G_DL_SYMBOL(
 	Program_InitWithString,
-	"_ZN5Caver7Program13InitWithStringERKNSt6__ndk112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEES8_",
+	"_ZN5Caver7Program14InitWithStringERKNSt6__ndk112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPS7_",
 	bool, (Program *this, String *src, String *err)
 )
 
 G_DL_SYMBOL(
 	Program_LoadFromProtobufMessage,
-	"_ZN5Caver7Program22LoadFromProtobufMessageERKN5Proto7ProgramE",
+	"_ZN5Caver7Program23LoadFromProtobufMessageERKNS_5Proto7ProgramE",
 	void, (Program *this, void *msg)
 )
 
 G_DL_SYMBOL(
 	Program_SaveToProtobufMessage,
-	"_ZNK5Caver7Program21SaveToProtobufMessageEPN5Proto7ProgramE",
+	"_ZNK5Caver7Program21SaveToProtobufMessageEPNS_5Proto7ProgramE",
 	void, (Program *this, void *msg)
 )
